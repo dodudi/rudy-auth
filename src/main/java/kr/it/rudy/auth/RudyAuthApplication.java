603b6a54b5724f88f13230b1637141580c7aed8f
Rudy <idejrdud@gmail.com>
2026-01-16 15:44:53 +0900

    first commit

---
+++ src/main/java/kr/it/rudy/auth/RudyAuthApplication.java
@@ -0,0 +1,13 @@
+package kr.it.rudy.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RudyAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RudyAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/it/rudy/auth/RudyAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.it.rudy.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RudyAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
